401807cdfd62e4109fd6da2a5cfa545f6bd38ac7
Liam <liammclaughlinis@gmail.com>
2022-10-28 15:56:18 -0600

    Landing page

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 from dotenv import dotenv_values
 from pymongo import MongoClient
 from userRoute import router as user_router
@@ -8,9 +9,29 @@ config = dotenv_values(".env")
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 async def root():
-    return {"message": "To begin navigate to /docs"}
+    """Upon loading server user will be navigated here.
+
+    Returns:
+        _type_: _description_
+    """
+    #return html resp
+    return """
+    <html>
+        <head>
+            <title>Python final proj</title>
+        </head>
+        <body>
+            <h1>Python final project.</h1>
+            <h3>By Liam McLaughlin</h3>
+            <p>
+            Please navigate to the docs for a user interface unless you intend to use a third party such as thunderclient/postman
+            </p>
+            <a href="/docs">Navigate to swagger GUI here..</a>
+        </body>
+    </html>  
+    """
 
 @app.on_event("startup")
 def startup_db_client():
